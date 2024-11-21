ea53c374cfe7325c538badfc0446eba6ca127d7c
Emmanuel-Sasere <emmanuelscript.code@gmail.com>
2024-11-21 17:29:03 +0100

    PROBLEM SOLVED

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import Hero from '../components/Home/Hero'
+import Hero from '../components/home/Hero'
 import SubHero from '../components/home/SubHero'
 import OurProject from '../components/home/OurProject'
 import Features from '../components/home/Features'
 import SubFeatures from '../components/home/SubFeatures'
 import ContactHome from '../components/home/ContactHome'
 import ContactHome2 from '../components/home/ContactHome2'
-import ViProp from '../components/VIProjects/ViProp'
+
 
 
 const Home = () => {
